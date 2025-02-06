ba8dd5f5c944fbf4331dbe9d7f8160517d8cd853
Sameliuk <sameliuk.anastasia@gmail.com>
2025-02-06 13:50:58 +0200

    lab 2

--- routes/student.js
+++ routes/student.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // Додайте себе сюди!!!!
-var students = [
+const students = [
     { id: 1, name: 'Максим', age: '19', bio: 'Живу в невеличкому містечку, багато чим цікавлюсь та основне це - програмування та кулінарія.', externalLink: 'https://discordapp.com/users/777127385551470602/', image: '/images/max.png' },
+    { id: 2, name: 'Анастасія', age: '18', bio: 'Люблю освоювати вміння', externalLink: 'https://www.linkedin.com/in/anastasia-sameliuk-48265129b/', image: '/images/nastya.jpg' },
 ];
 
 router.get('/:id', function (req, res) {
-    var student = students.find(s => s.id == req.params.id);
+    const student = students.find(s => s.id == req.params.id);
     if (!student) {
         return res.status(404).send('Сторінки не знайдено!')
     }
