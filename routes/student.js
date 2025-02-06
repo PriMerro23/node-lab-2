59305e819e0c072f1869bd466de67ec5ca7c398e
Sameliuk <86837651+Sameliuk@users.noreply.github.com>
2025-02-06 13:52:36 +0200

    Update student.js

--- routes/student.js
+++ routes/student.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Додайте себе сюди!!!!
 const students = [
     { id: 1, name: 'Максим', age: '19', bio: 'Живу в невеличкому містечку, багато чим цікавлюсь та основне це - програмування та кулінарія.', externalLink: 'https://discordapp.com/users/777127385551470602/', image: '/images/max.png' },
-    { id: 2, name: 'Анастасія', age: '18', bio: 'Люблю освоювати вміння', externalLink: 'https://www.linkedin.com/in/anastasia-sameliuk-48265129b/', image: '/images/nastya.jpg' },
+    { id: 2, name: 'Анастасія', age: '18', bio: 'Люблю освоювати нові вміння', externalLink: 'https://www.linkedin.com/in/anastasia-sameliuk-48265129b/', image: '/images/nastya.jpg' },
 ];
 
 router.get('/:id', function (req, res) {
